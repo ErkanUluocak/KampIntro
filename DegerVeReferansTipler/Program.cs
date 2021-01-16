c6cd3fc8e63f77c5d087b9e7d12d1473b217a8f2
Erkan Uluocak <erkan.uluocak@gmail.com>
2021-01-16 23:47:22 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            Console.WriteLine(sayi1);
+            Console.WriteLine(sayi2);
+
+
+
+
+
+
+
+            //int, decimal, float, double, bool = değer tip
+            //array, class, interface = referans tip
+
+            //class'ta new yaptığı yeri
+            //referans tipleri tekrar bak
+
+
+        }
+    }
+}
---
+++ Metematik/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
